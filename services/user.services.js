70f73d2cbfa6f10e02b7b73293f71c690c0edfec
RamyElgharbawy <ra.elgharbawy@gmail.com>
2025-05-06 13:59:17 +0300

    add pagination feature for get all service

--- services/user.services.js
+++ services/user.services.js
@@ -28,7 +28,20 @@ exports.createUser = asyncHandler(async (req, res, next) => {
 // @route     GET /api/v1/users
 // @access    Private
 exports.getAllUser = asyncHandler(async (req, res, next) => {
+  // pagination options
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || 10;
+  const skip = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  // get all user count
+  const allUserCount = await prisma.user.count();
+
+  // get all user data
   const users = await prisma.user.findMany({
+    skip,
+    take: limit,
+    orderBy: { createdAt: "desc" },
     select: {
       id: true,
       name: true,
@@ -42,7 +55,17 @@ exports.getAllUser = asyncHandler(async (req, res, next) => {
     return next(new ApiError(`there is no users`, 404));
   }
 
-  res.status(201).json({ results: users.length, data: users });
+  // pagination result
+  const pagination = {};
+
+  pagination.totalItems = allUserCount;
+  pagination.currentPage = page;
+  pagination.itemsPerPage = limit;
+  pagination.totalPages = Math.ceil(allUserCount / limit);
+  pagination.prevPage = skip > 0 ? page - 1 : null;
+  pagination.nextPage = endIndex < allUserCount ? page + 1 : null;
+
+  res.status(200).json({ pagination, data: users });
 });
 
 // @desc      Get Specific User Service
@@ -66,7 +89,7 @@ exports.getUser = asyncHandler(async (req, res, next) => {
     return next(new ApiError(`there is no user for this id: ${userId}`, 404));
   }
 
-  res.status(201).json({ data: user });
+  res.status(200).json({ data: user });
 });
 
 // @desc      Update Specific user Service
